813a9303fc70dabab0fc00d887f7dec92679bd29
KevinBelien <K3vin.belien@gmail.com>
2020-10-21 14:33:49 +0200

    Initial create project

---
+++ For_Realty/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace For_Realty.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
